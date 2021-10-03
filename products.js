91ca432e2e7fee3fdf97d4e3870b2fe2684370b4
codu2 <82283517+codu2@users.noreply.github.com>
2021-10-04 04:45:52 +0900

    Add files via upload

---
+++ products.js
@@ -0,0 +1,106 @@
+let products = [
+    {
+        name: 'Converse x Tyler Artist Series',
+        image1: 'img/collaboration.jpg',
+        image2: 'img/collaboration2.jpg',
+        old_price: '115,000',
+        curr_price: '105,000'
+    },
+    {
+        name: 'Converse x Basquiat',
+        image1: 'img/collaboration3.jpeg',
+        image2: 'img/collaboration4.jpeg',
+        old_price: '125,000',
+        curr_price: '115,000'
+    },
+    {
+        name: 'Converse x Comme Des Garçons Play',
+        image1: 'img/collaboration5.jpeg',
+        image2: 'img/collaboration6.jpeg',
+        old_price: '198,000',
+        curr_price: '178,000'
+    },
+    {
+        name: 'Converse x Joe Freshgoods',
+        image1: 'img/collaboration7.jpeg',
+        image2: 'img/collaboration8.jpeg',
+        old_price: '140,000',
+        curr_price: '130,000'
+    },
+    {
+        name: 'Converse x Tyler Artist Series',
+        image1: 'img/collaboration.jpg',
+        image2: 'img/collaboration2.jpg',
+        old_price: '115,000',
+        curr_price: '105,000'
+    },
+    {
+        name: 'Converse x Basquiat',
+        image1: 'img/collaboration3.jpeg',
+        image2: 'img/collaboration4.jpeg',
+        old_price: '125,000',
+        curr_price: '115,000'
+    },
+    {
+        name: 'Converse x Comme Des Garçons Play',
+        image1: 'img/collaboration5.jpeg',
+        image2: 'img/collaboration6.jpeg',
+        old_price: '198,000',
+        curr_price: '178,000'
+    },
+    {
+        name: 'Converse x Joe Freshgoods',
+        image1: 'img/collaboration7.jpeg',
+        image2: 'img/collaboration8.jpeg',
+        old_price: '140,000',
+        curr_price: '130,000'
+    },
+]
+
+let product_list = document.querySelector('#products')
+
+renderProducts = (products) => {
+    products.forEach(e => {
+        let prod = `
+                        <div class="col-4 col-md-6 col-sm12">
+                            <div class="product-card">
+                                <div class="product-card-img">
+                                    <img src="${e.image1}" alt="">
+                                    <img src="${e.image2}" alt="">
+                                </div>
+                                <div class="product-card-info">
+                                    <div class="product-btn">
+                                        <a href="./product-detail.html" class="btn-flat btn-hover btn-shop-now">shop now</a>
+                                        <button class="btn-flat btn-hover btn-cart-add">
+                                            <i class='bx bxs-cart-add'></i>
+                                        </button>
+                                        <button class="btn-flat btn-hover btn-cart-add">
+                                            <i class='bx bxs-heart'></i>
+                                        </button>
+                                    </div>
+                                    <div class="product-card-name">
+                                        ${e.name}
+                                    </div>
+                                    <div class="product-card-price">
+                                        <span><del>${e.old_price}</del></span>
+                                        <span class="curr-price">${e.curr_price}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `
+        product_list.insertAdjacentHTML("beforeend", prod)
+    })
+}
+
+renderProducts(products);
+
+let filter_col = document.querySelector('#filter-col');
+
+document.querySelector('#filter-toggle').addEventListener('click', () => {
+    filter_col.classList.toggle('active');
+})
+
+document.querySelector('#filter-close').addEventListener('click', () => {
+    filter_col.classList.toggle('active');
+})
