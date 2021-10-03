e3ba33f8a2b6b811190f6e479f45a35ccaae2ac2
codu2 <82283517+codu2@users.noreply.github.com>
2021-10-04 04:48:13 +0900

    Update and rename main.js to js/main.js

--- js/main.js
+++ js/main.js
@@ -139,19 +139,3 @@ products.forEach(e => {
     product_list.insertAdjacentHTML("beforeend", prod);
     best_product_list.insertAdjacentHTML("afterbegin", prod);
 })
-
-document.querySelectorAll('.dropdown > a').forEach(e => {
-    e.addEventListener('click', (event) => event.preventDefault())
-})
-
-document.querySelectorAll('.mega-dropdown > a').forEach(e => {
-    e.addEventListener('click', (event) => event.preventDefault())
-})
-
-document.querySelector('#mb-menu-toggle').addEventListener('click', () => {
-    document.querySelector('#header-wrapper').classList.add('active')
-})
-
-document.querySelector('#mb-menu-close').addEventListener('click', () => {
-    document.querySelector('#header-wrapper').classList.remove('active')
-})
